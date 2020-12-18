3bf90b9cf4de4103973f44771fe76b67199e46b1
Andrew Koidan <deathangel908@gmail.com>
2020-12-19 00:16:05 +0200

    1.2.6

--- index.js
+++ index.js
@@ -456,7 +456,7 @@ function Painter(containerPaitner, conf) {
       var check = [];
       self.keyProcessors.forEach(function (proc) {
         if (check.indexOf(proc.code) >= 0) {
-          throw "key " + proc.code + "is used";
+          throw Error("key " + proc.code + "is used");
         }
         check.push(proc.code);
       });
@@ -1177,110 +1177,79 @@ function Painter(containerPaitner, conf) {
       };
       tool.onChangeFillOpacity = function(e) {};
       tool.floodFill = (function() {
-        var called;
-        var xssss;
-        var yssss;
-        var fillcolorssss;
-        function floodfill(data, x, y, fillcolor, tolerance, width, height) {
-          called = 0;
-          xssss = x;
-          yssss = y;
-          fillcolorssss = fillcolor;
-          var length = data.length;
-          var Q = [];
-          var i = (x + y * width) * 4;
-          var e = i, w = i, me, mw, w2 = width * 4;
-          var targetcolor = [data[i], data[i + 1], data[i + 2], data[i + 3]];
-          if (!pixelCompare(i, targetcolor, fillcolor, data, length, tolerance)) {
-            return false;
+        /** Flood fill algorithm based on gman stackoverflow answer
+         * https://stackoverflow.com/a/56221940/3872976
+         */
+        function getPixel(x, y, width, height, data) {
+          if (x < 0 || y < 0 || x >= width || y >= height) {
+            return -1;
+          } else {
+            return data[y * width + x];
           }
-          Q.push(i);
-          while (Q.length) {
-            i = Q.pop();
-            if (pixelCompareAndSet(i, targetcolor, fillcolor, data, length, tolerance)) {
-              e = i;
-              w = i;
-              mw = parseInt(i / w2) * w2; //left bound
-              me = mw + w2;             //right bound
-              while (mw < w && mw < (w -= 4) && pixelCompareAndSet(w, targetcolor, fillcolor, data, length, tolerance)); //go left until edge hit
-              while (me > e && me > (e += 4) && pixelCompareAndSet(e, targetcolor, fillcolor, data, length, tolerance)); //go right until edge hit
-              for (var j = w; j < e; j += 4) {
-                if (j - w2 >= 0 && pixelCompare(j - w2, targetcolor, fillcolor, data, length, tolerance)) Q.push(j - w2); //queue y-1
-                if (j + w2 < length && pixelCompare(j + w2, targetcolor, fillcolor, data, length, tolerance)) Q.push(j + w2); //queue y+1
-              }
+        }
+
+
+        function doWhile(pixelsToCheck, width, height, data, targetColor, fillColor, cb) {
+          var tickCount = 1;
+          while (pixelsToCheck.length > 0) {
+            if (tickCount % 5000 === 0) { // do not block event loop
+              // do not block event loop
+              logger.debug("Postponing action")();
+              setTimeout(doWhile, 0, pixelsToCheck, width, height, data, targetColor, fillColor, cb);
+              return;
             }
+            var y = pixelsToCheck.pop();
+            var x = pixelsToCheck.pop();
+            var currentColor = getPixel(x, y, width, height, data);
+            if (currentColor === targetColor) {
+              tickCount++;
+              data[y * width + x] = fillColor;
+              pixelsToCheck.push(x + 1, y);
+              pixelsToCheck.push(x - 1, y);
+              pixelsToCheck.push(x, y + 1);
+              pixelsToCheck.push(x, y - 1);
+            }
+          }
+          cb();
+        }
+
+        function floodFill(data, x, y, fillColor, width, height, cb) {
+
+          // get the color we're filling
+          var targetColor = getPixel(x,y, width,height, data);
+
+          // check we are actually filling a different color
+          if (targetColor !== fillColor) {
+            doWhile([x, y], width, height, data, targetColor, fillColor, cb);
           }
           return data;
         }
-
-        function pixelCompare(i, targetcolor, fillcolor, data, length, tolerance) {
-          if (i < 0 || i >= length) return false; //out of bounds
-          if (data[i + 3] === 0 && fillcolor.a > 0) return true;  //surface is invisible and fill is visible
-
-          if (
-            Math.abs(targetcolor[3] - fillcolor.a) <= tolerance &&
-            Math.abs(targetcolor[0] - fillcolor.r) <= tolerance &&
-            Math.abs(targetcolor[1] - fillcolor.g) <= tolerance &&
-            Math.abs(targetcolor[2] - fillcolor.b) <= tolerance
-          ) return false; //target is same as fill
-
-          if (
-            (targetcolor[3] === data[i + 3]) &&
-            (targetcolor[0] === data[i]  ) &&
-            (targetcolor[1] === data[i + 1]) &&
-            (targetcolor[2] === data[i + 2])
-          ) return true; //target matches surface
-
-          if (
-            Math.abs(targetcolor[3] - data[i + 3]) <= (255 - tolerance) &&
-            Math.abs(targetcolor[0] - data[i]) <= tolerance &&
-            Math.abs(targetcolor[1] - data[i + 1]) <= tolerance &&
-            Math.abs(targetcolor[2] - data[i + 2]) <= tolerance
-          ) return true; //target to surface within tolerance
-
-          return false; //no match
-        }
-
-        function pixelCompareAndSet(i, targetcolor, fillcolor, data, length, tolerance) {
-          called++;
-          if (called > 10000000) {
-            throw "Unable to flood fill the image, because cycle detected.";
-          }
-          if (pixelCompare(i, targetcolor, fillcolor, data, length, tolerance)) {
-            //fill the color
-            data[i] = fillcolor.r;
-            data[i + 1] = fillcolor.g;
-            data[i + 2] = fillcolor.b;
-            data[i + 3] = fillcolor.a;
-            return true;
-          }
-          return false;
-        }
-        return floodfill;
+        return floodFill;
       })();
       tool.getRGBA = function () {
         var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(self.ctx.fillStyle);
+        var r = parseInt(result[1], 16);
+        var g = parseInt(result[2], 16);
+        var b =parseInt(result[3], 16);
+        var a =(self.instruments.opacityFill.inputValue || 0) * 255;
         if (!result) {
-          throw "Invalid color";
+          throw Error("Invalid color");
         }
-        return {
-          r: parseInt(result[1], 16),
-          g: parseInt(result[2], 16),
-          b: parseInt(result[3], 16),
-          a: (self.instruments.opacityFill.inputValue || 0) * 255
-        }
+        return a << 24 | (b << 16) | (g << 8) | r;
       };
       tool.onMouseDown = function (e) {
-        if (!((self.dom.canvas.width * self.dom.canvas.height) < 1000001)) {
-          logger.debug("Can't fill image because amount of  data is too huge. Your browser would just explode ;(");
+        if (!((self.dom.canvas.width * self.dom.canvas.height) < 4000001)) {
+          alert("Can't flood fill, because your browser is unable to process canvas size " + self.dom.canvas.width + "x" + self.dom.canvas.height+ ", which is more than 4kk pixels.");
         } else {
           var xy = self.helper.getXY(e);
           var image = self.buffer.startAction();
-          var processData = image.data.slice(0);
-          tool.floodFill(processData, xy.x, xy.y, tool.getRGBA(), 0, image.width, image.height);
-          var resultingImg = new ImageData(processData, image.width, image.height);
-          self.ctx.putImageData(resultingImg, 0, 0);
-          self.buffer.finishAction(resultingImg);
+          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8ClampedArray#Browser_compatibility
+          var processData = new Uint32Array(image.data.slice(0).buffer); // clone data,so we won't modify history
+          tool.floodFill(processData, xy.x, xy.y, tool.getRGBA(), image.width, image.height, function() {
+            var resultingImg = new ImageData(new Uint8ClampedArray(processData.buffer), image.width, image.height);
+            self.ctx.putImageData(resultingImg, 0, 0);
+            self.buffer.finishAction(resultingImg);
+          });
         }
       }
     })(),
@@ -1786,7 +1755,7 @@ function Painter(containerPaitner, conf) {
     var redoImages = [];
     // var paintUndo = $('paintUndo');
     // var paintRedo = $('paintRedo');
-    var buStateData = ['lineWidth', 'strokeStyle', 'globalAlpha', 'lineJoin', 'lineCap', 'globalCompositeOperation'];
+    var buStateData = ['lineWidth', 'fillStyle', 'strokeStyle', 'fontFamily', 'font', 'globalAlpha', 'lineJoin', 'lineCap', 'globalCompositeOperation'];
     var current = null;
     tool.getCanvasImage = function (img) {
       return {
