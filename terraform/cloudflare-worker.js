518acaccfdc8965c7967c6786973de4d68907ab5
Andrew Koidan <deathangel908@gmail.com>
2023-07-16 00:15:27 +0200

    Update cloudflare-worker.js

--- terraform/cloudflare-worker.js
+++ terraform/cloudflare-worker.js
@@ -1,18 +1,29 @@
-function makeid(length = 10) {
+function makeid(length) {
+    let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    return Array.from(
-        {length},
-        () => characters.charAt(Math.floor(Math.random() * characters.length))
-    ).join('')
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
 }
-
+function base64ToArrayBuffer(base64) {
+    var binaryString = atob(base64);
+    var bytes = new Uint8Array(binaryString.length);
+    for (var i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
 
 async function uploadFileAndGetResponse(request, env) {
     // https://github.com/TomasHubelbauer/mime-multipart/issues/1
     let body = await request.formData();
     let file = await body.get('file');
     const blob = await file.arrayBuffer();
-    const id = makeid() + ".png";
+    const id = makeid(10) + ".png";
     await env.spainter.put(id, blob, {
         httpMetadata: {
             "contentType": file.type,
@@ -31,6 +42,12 @@ export default {
     async fetch(request, env) {
         if (request.method === 'POST') {
             return await uploadFileAndGetResponse(request, env);
+        } else if (request.url.endsWith('favicon.ico')) {
+            return new Response(base64ToArrayBuffer(env.favicon), {
+                headers: {
+                    "content-type": "image/x-icon",
+                },
+            }); 
         } else {
             return new Response(env.html, {
                 headers: {
